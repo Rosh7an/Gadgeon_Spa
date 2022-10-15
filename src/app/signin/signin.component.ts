2a8aec32baf6e980e8ccf3e3e2ccf912a0f47896
Rosh7an <roshanj0742@gmail.com>
2022-10-15 18:10:24 +0530

    login validation commit

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -6,8 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
+  loginForm!:FormGroup
+  constructor(private formbuilder:FormBuilder, private http:HttpClient,private router:Router) { 
+    this.loginForm=formbuilder.group({
+      email:[''],
+      pass:[''],
+      
+    })
+  }
 
-  constructor() { }
+  signin(){
+    this.http.get<any>("http://localhost:3000/users").subscribe(res=>
+    {
+      const users=res.find((a:any)=>{
+        return a.email===this.loginForm.value.email && a.pass===this.loginForm.value.pass
+      })
+      if(users){
+        alert("User Exists")
+      this.loginForm.reset
+      }
+    })
+   }
 
   ngOnInit(): void {
   }
